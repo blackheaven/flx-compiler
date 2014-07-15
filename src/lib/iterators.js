38e56c60c133ff5315e70c6f8a2f87c384a530ba
Gautier DI FOLCO <gautier.difolco@gmail.com>
2014-07-15 10:57:17 +0200

    Refactoring src/lib/iterators.js

--- src/lib/iterators.js
+++ src/lib/iterators.js
@@ -1,11 +1,13 @@
-module.exports = function iteratorFactory(types) {  
+var errors = require('./errors');
+
+module.exports = function iteratorFactory(types) {
     return function iterator(c) {
         function handlerFactory(type) {
-            return function handler(n) {
-                if (!n.type)
-                    throw errors.missingType(n);
-                if (!!types[n.type] && types[n.type][type])
-                    return types[n.type][type](n, c);
+            return function handler(node, previousNode) {
+                if (!node.type)
+                    throw errors.missingType(node);
+                if (!!types[node.type] && types[node.type][type])
+                    return types[node.type][type](c, node, previousNode);
             };
         }
 
@@ -14,4 +16,4 @@ module.exports = function iteratorFactory(types) {
             leave: handlerFactory('leave')
         };
     };
-}
+};
